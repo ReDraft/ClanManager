af19a7e16d73b94c718d980347c60cb74f2acf17
ReDraft <support@redraft.de>
2017-01-24 08:48:43 +0100

    Added minimal Packages
    barryvdh/laravel-ide-helper
    barryvdh/laravel-debugbar
    doctrine/dbal
    laravelcollective/html

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // load only in development
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
+
+        // always load these
+        $this->app->register(\Collective\Html\HtmlServiceProvider::class);
+        $this->app->alias('Form', \Collective\Html\FormFacade::class);
+        $this->app->alias('Html', \Collective\Html\HtmlFacade::class);
+
     }
 }
